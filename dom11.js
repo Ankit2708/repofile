1577d7f5f12482b3d374ca806c6dacc046b7cc83
Ankit2708 <shivhare.ankit476@gmail.com>
2022-03-11 01:37:23 +0530

    ankit2708/dom12

---
+++ dom11.js
@@ -0,0 +1,26 @@
+// //console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// //document.title = 123;
+// console.log(document.body);
+// console.log(document.head);
+// console.log(document.all);
+// console.log(document.all[10]);
+// //document.all[10].textContent='Hello';
+// console.log(document.forms);
+// console.log(document.links);
+//console.log(document.images);
+//Get elementbyid
+//console.log(document.getElementById('header-title'));
+let headerTitle=document.getElementById('header-title');
+let header=document.getElementById('main-header');
+//console.log(headerTitle);
+// headerTitle.textContent='Hello';
+// headerTitle.innerText='Goodbye';
+// console.log(headerTitle.innerText);
+//headerTitle.innerHTML='<h3>Hello</h3';
+//headerTitle.style.borderBottom='solid 3px #000';
+//header.style.borderBottom='solid 3px #000';
+let items=document.getElementsByClassName('title')[0];
+items.style.color="green";
